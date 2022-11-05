63296c361e328201f3eeeb7d9cb8fa06beea4599
Florian Dejonckheere <florian@floriandejonckheere.be>
2022-11-05 18:06:04 +0200

    Serve direct path

--- app/serializers/image_serializer.rb
+++ app/serializers/image_serializer.rb
@@ -7,7 +7,8 @@ class ImageSerializer < CacheCrispies::Base
   serialize :id
 
   serialize :url do |model, _options|
-    Rails.application.routes.url_helpers.rails_blob_path(model, only_path: true)
+    ActiveStorage::Current.url_options = { host: "localhost:3000" }
+    model.blob.url.remove("http://localhost:3000")
   rescue
     nil
   end
