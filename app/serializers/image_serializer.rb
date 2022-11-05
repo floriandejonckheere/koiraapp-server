ead71f286c4ceac12eefef1ad23bf48379256b8b
Florian Dejonckheere <florian@floriandejonckheere.be>
2022-11-05 16:28:16 +0200

    Check if attached

--- app/serializers/image_serializer.rb
+++ app/serializers/image_serializer.rb
@@ -7,7 +7,7 @@ class ImageSerializer < CacheCrispies::Base
   serialize :id
 
   serialize :url do |model, _options|
-    next unless model.attached?
+    next unless model
 
     Rails.application.routes.url_helpers.rails_blob_path(model, only_path: true)
   end
